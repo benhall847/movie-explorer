f1bc14ddbe9ad7f65f6632f2cd33e7bd12bc477d
Benjamin Hallquist <benhall84707@gmail.com>
2019-03-14 13:32:35 -0400

    changed function names

--- scripts/index.js
+++ scripts/index.js
@@ -1,40 +1,50 @@
 const listArea = document.querySelector('[data-list]');
 
-const imgURL = 'https://image.tmdb.org/t/p/w200'
+const imgURL = 'https://image.tmdb.org/t/p/w200';
 
-let trendingList
+let trendingList;
 
 
 
 function posterClick(event){
     console.log(event.target.data);
-}
+};
 
 
-function doStuff(obj){
+function createIMGelement(obj){
     const aDiv = document.createElement('div');
     const myImg = document.createElement('img');
     listArea.appendChild(aDiv);
-    myImg.setAttribute('src', `${imgURL}${obj.poster_path}`)
-    aDiv.setAttribute('style', 'width: 33%')
-    myImg.setAttribute('style', 'width: 98%')
-    myImg.data = obj
+    myImg.setAttribute('src', `${imgURL}${obj.poster_path}`);
+    aDiv.setAttribute('class', 'poster-frame');
+    myImg.setAttribute('class', 'poster-image');
+    myImg.data = obj;
     aDiv.appendChild(myImg);
-    myImg.addEventListener('click', posterClick)
+    myImg.addEventListener('click', posterClick);
 };
 
-function doLotsStuff(myArray){
-    myArray.forEach(eaObj => {
-        doStuff(eaObj)
+function createElements(myArray){
+    myArray.forEach(movieObj => {
+        createIMGelement(movieObj);
     })
 };
 
+function sortPopularity(a, b) {
+    if (a.popularity > b.popularity){
+        return -1;
+    }
+    if (b.popularity > a.popularity){
+        return 1;
+    }
+    return 0;
+};
 
 const ListSpike = fetch("https://api.themoviedb.org/3/trending/all/week?api_key=a2fe439608a4e1ab4fe40ea29bac0e9e")
     .then(response =>{
         return response.json();
     })
     .then(data =>{
-        trendingList = data.results
-        doLotsStuff(trendingList);
+        trendingList = data.results;
+        trendingList.sort(sortPopularity);
+        createElements(trendingList);
 });
