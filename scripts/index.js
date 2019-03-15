003ce021aa8ad0967009e24e72266822b3380a47
Donald Yeh <Donald.Yeh@gmail.com>
2019-03-15 10:33:36 -0400

    prettied syntax

--- scripts/index.js
+++ scripts/index.js
@@ -5,7 +5,7 @@ const imgURL = "https://image.tmdb.org/t/p/w200";
 let trendingList;
 
 const targetInput = document.querySelector("[data-searchBar]"),
-  results = document.querySelector("[data-autocomplete-results]");
+    results = document.querySelector("[data-autocomplete-results]");
 
 let matches = [];
 
@@ -13,87 +13,87 @@ let matches = [];
 targetInput.focus();
 
 function getMatches(inputText) {
-  let matchList = [];
-  let lowerCasedInputText = inputText.toLowerCase();
-  console.log(`inside getMatches, trendingList: ${trendingList}`);
+    let matchList = [];
+    let lowerCasedInputText = inputText.toLowerCase();
+    console.log(`inside getMatches, trendingList: ${trendingList}`);
 
-  trendingList.forEach(movie => {
-    if (movie.title) {
-      // will only search through movies, not shows.
-      // if the movie title [string] contains part of the input text
-      if (movie.title.toLowerCase().indexOf(lowerCasedInputText) > -1) {
-        matchList.push(movie);
-      }
-    }
-  });
-  return matchList; // array of matches
+    trendingList.forEach(movie => {
+        if (movie.title) {
+            // will only search through movies, not shows.
+            // if the movie title [string] contains part of the input text
+            if (movie.title.toLowerCase().indexOf(lowerCasedInputText) > -1) {
+                matchList.push(movie);
+            }
+        }
+    });
+    return matchList; // array of matches
 }
 
 // populate autocomplete results, accepts matchList array as an argument
 function displayMatches(matchList) {
-  matchList.forEach(match => {
-    const liElement = document.createElement("li");
-    liElement.textContent = match.title;
-    results.appendChild(liElement);
-  });
+    matchList.forEach(match => {
+        const liElement = document.createElement("li");
+        liElement.textContent = match.title;
+        results.appendChild(liElement);
+    });
 }
 
 function posterClick(event) {
-  console.log(event.target.data);
+    console.log(event.target.data);
 }
 
 function createIMGelement(obj) {
-  const aDiv = document.createElement("div");
-  const myImg = document.createElement("img");
-  listArea.appendChild(aDiv);
-  myImg.setAttribute("src", `${imgURL}${obj.poster_path}`);
-  aDiv.setAttribute("class", "poster-frame");
-  myImg.setAttribute("class", "poster-image");
-  myImg.data = obj;
-  aDiv.appendChild(myImg);
-  myImg.addEventListener("click", posterClick);
+    const aDiv = document.createElement("div");
+    const myImg = document.createElement("img");
+    listArea.appendChild(aDiv);
+    myImg.setAttribute("src", `${imgURL}${obj.poster_path}`);
+    aDiv.setAttribute("class", "poster-frame");
+    myImg.setAttribute("class", "poster-image");
+    myImg.data = obj;
+    aDiv.appendChild(myImg);
+    myImg.addEventListener("click", posterClick);
 }
 
 function createElements(myArray) {
-  myArray.forEach(movieObj => {
-    createIMGelement(movieObj);
-  });
+    myArray.forEach(movieObj => {
+        createIMGelement(movieObj);
+    });
 }
 
 function sortPopularity(a, b) {
-  if (a.popularity > b.popularity) {
-    return -1;
-  }
-  if (b.popularity > a.popularity) {
-    return 1;
-  }
-  return 0;
+    if (a.popularity > b.popularity) {
+        return -1;
+    }
+    if (b.popularity > a.popularity) {
+        return 1;
+    }
+    return 0;
 }
 
 const ListSpike = fetch(
-  "https://api.themoviedb.org/3/trending/all/week?api_key=a2fe439608a4e1ab4fe40ea29bac0e9e"
+    "https://api.themoviedb.org/3/trending/all/week?api_key=a2fe439608a4e1ab4fe40ea29bac0e9e"
 )
-  .then(response => {
-    return response.json();
-  })
-  .then(data => {
-    trendingList = data.results;
-    trendingList.sort(sortPopularity);
-    createElements(trendingList);
-  });
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        trendingList = data.results;
+        trendingList.sort(sortPopularity);
+        createElements(trendingList);
+    });
 
 function search(event) {
-  const searchInput = event.srcElement.value;
-  results.innerHTML = "";
-  fetch(
-    `https://api.themoviedb.org/3/search/movie?api_key=a2fe439608a4e1ab4fe40ea29bac0e9e&language=en-US&query=${searchInput}&page=1&include_adult=false`
-  );
-  if (event.srcElement.value.length > 0) {
-    matches = getMatches(event.srcElement.value);
-    if (matches.length > 0) {
-      displayMatches(matches);
+    const searchInput = event.srcElement.value;
+    results.innerHTML = "";
+    fetch(
+        `https://api.themoviedb.org/3/search/movie?api_key=a2fe439608a4e1ab4fe40ea29bac0e9e&language=en-US&query=${searchInput}&page=1&include_adult=false`
+    );
+    if (event.srcElement.value.length > 0) {
+        matches = getMatches(event.srcElement.value);
+        if (matches.length > 0) {
+            displayMatches(matches);
+        }
     }
-  }
 }
 
 targetInput.addEventListener("keyup", search);
