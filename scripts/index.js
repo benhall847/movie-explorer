5f67405c7d72019c133e1e78bab77a58f22bc48e
Donald Yeh <Donald.Yeh@gmail.com>
2019-03-15 09:08:35 -0400

    creates an array of matches from input

--- scripts/index.js
+++ scripts/index.js
@@ -7,7 +7,62 @@ let trendingList;
 const targetInput = document.querySelector("[data-searchBar]"),
   results = document.querySelector("[data-autocomplete-results]");
 
+let matches = [];
+
+// focus the input
 targetInput.focus();
+
+// displays autocomplete results
+
+// toggles the results list
+function toggleResults(action) {
+  if (action == "show") {
+    results.classList.add("visible");
+  } else if (action == "hide") {
+    results.classList.remove("visible");
+  }
+}
+
+function getMatches(inputText) {
+  let matchList = [];
+  let lowerCasedInputText = inputText.toLowerCase();
+
+  //   console.log("in getMatches");
+  //   console.log(inputText);
+  //   console.log(lowerCasedInputText);
+  console.log(`inside getMatches, trendingList: ${trendingList}`);
+
+  trendingList.forEach(movie => {
+    console.log(movie);
+    console.log(movie.title);
+    if (movie.title) {
+      // will only search through movies, not shows.
+      // if the movie title [string] contains part of the input text
+      if (movie.title.toLowerCase().indexOf(lowerCasedInputText) > -1) {
+        matchList.push(movie.title);
+      }
+    }
+  });
+
+  console.log(matchList);
+  //   displayMatches(matchList);
+  return matchList; // array of matches
+} // end of getMatches
+
+// populate autocomplete results, accepts matchList array as an argument
+function displayMatches(matchList) {
+  matchList.forEach(match => {
+    const liElement = document.createElement("li");
+    liElement.classList.add("result");
+    const ulElement = document.querySelector("[data-searchButton]");
+    // console.log(`in displayMatches, matchList: ${matchList}`);
+    // console.log(matchList);
+    liElement.textContent = match;
+    // console.log(liElement);
+    ulElement.appendChild(liElement);
+    toggleResults("show");
+  });
+}
 
 function posterClick(event) {
   console.log(event.target.data);
@@ -51,4 +106,26 @@ const ListSpike = fetch(
     trendingList = data.results;
     trendingList.sort(sortPopularity);
     createElements(trendingList);
+
+    targetInput.addEventListener("keyup", function(event) {
+      console.log(results);
+      results.innerHTML = "";
+      toggleResults("hide");
+      // `this` refers to the `targetInput`
+      if (this.value.length > 0) {
+        // if we typed something
+        console.log("inside the if inside addEventListener");
+        // console.log(event);
+        console.log(event.srcElement.value); // input value
+        matches = getMatches(event.srcElement.value);
+        if (matches.length > 0) {
+          displayMatches(matches);
+        }
+
+        // if (matches.length > 0) {
+        //   displayMatches(matches);
+        // }
+      }
+      //   getMatches(trendingList);
+    }); // ends addEventListener
   });
